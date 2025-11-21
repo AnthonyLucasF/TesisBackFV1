e7249090e1b9d1bda1f5d738b474094c35f12759
Anthony Lucas Floreano <anthony.lucasfloreano@upse.edu.ec>
2025-11-20 19:28:21 -0500

    Corrección de errores menores

--- src/routes/ingresotunel.routes.js
+++ src/routes/ingresotunel.routes.js
@@ -1,10 +1,12 @@
+// src/routes/ingresotunel.routes.js
 import { Router } from "express";
-import { getIngresoTunel, getIngresoTunelPorLote, getIngresoTunelxid, postIngresoTunel, putIngresoTunel, deleteIngresoTunel } from '../controladores/ingresotunelCtrl.js'
+import { getIngresoTunel, getIngresoTunelPorLote, getIngresoTunelxid, getRendimientoLote, postIngresoTunel, putIngresoTunel, deleteIngresoTunel } from '../controladores/ingresotunelCtrl.js'
 
 const router = Router()
 
 router.get('/ingresotunel', getIngresoTunel)
 router.get('/ingresotunel/porlote/:lote_id', getIngresoTunelPorLote)
+router.get('/ingresotunel/rendimiento/:lote_id', getRendimientoLote)
 router.get('/ingresotunel/:id', getIngresoTunelxid)
 router.post('/ingresotunel', postIngresoTunel)
 router.put('/ingresotunel/:id', putIngresoTunel)
